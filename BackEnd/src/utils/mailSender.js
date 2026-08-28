bf5b88d810fe59202ef7ec3c681a27e0ad7586bb
RabiX07 <149926560+RabiX07@users.noreply.github.com>
2026-08-28 23:24:15 +0530

    nodemailer port specified

--- BackEnd/src/utils/mailSender.js
+++ BackEnd/src/utils/mailSender.js
@@ -4,25 +4,26 @@ const mailSender = async (email, body, subject) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-      }
+        pass: process.env.MAIL_PASS,
+      },
     });
 
     const mailOptions = {
       from: process.env.MAIL_USER,
       to: email,
       subject: subject,
-      html: body
+      html: body,
     };
 
     const info = await transporter.sendMail(mailOptions);
 
     return info;
-
   } catch (error) {
-    console.error('Error sending mail:', error);
+    console.error("Error sending mail:", error);
     throw error;
   }
 };
