3f03e9e2f79289d86d2ccaa22abe095c9c828b97
RabiX07 <149926560+RabiX07@users.noreply.github.com>
2026-08-28 23:15:57 +0530

    mail

--- BackEnd/src/utils/mailSender.js
+++ BackEnd/src/utils/mailSender.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from "nodemailer";
 
 const mailSender = async (email, body, subject) => {
   try {
@@ -27,4 +27,4 @@ const mailSender = async (email, body, subject) => {
   }
 };
 
-module.exports = mailSender;
+export default mailSender;
