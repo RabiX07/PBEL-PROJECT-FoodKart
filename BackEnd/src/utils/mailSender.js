0daf781462cf9e732af9bb61d9b4e350b5f44465
RabiX07 <149926560+RabiX07@users.noreply.github.com>
2026-08-29 00:35:54 +0530

    port 465 mail

--- BackEnd/src/utils/mailSender.js
+++ BackEnd/src/utils/mailSender.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
 
     connectionTimeout: 10000,
     greetingTimeout: 10000,
