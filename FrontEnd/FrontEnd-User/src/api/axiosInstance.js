9afef63eb9a02792df1bd4011044089baee0fc4a
RabiX07 <149926560+RabiX07@users.noreply.github.com>
2026-09-01 19:06:52 +0530

    test2

--- FrontEnd/FrontEnd-User/src/api/axiosInstance.js
+++ FrontEnd/FrontEnd-User/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: `http://${window.location.hostname}:3000/api`,
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `/api`,
   withCredentials: true,     // allow cookies (important for JWT)
   headers: {
     "Content-Type": "application/json",
